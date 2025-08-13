c075d9a2fe5d5384792df94fccab8e05f3992ed6
BitMutton <bitmutton@gmail.com>
2025-08-13 21:18:21 +0500

    Filament starters

---
+++ app/Filament/Resources/AcademicYears/Schemas/AcademicYearForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\AcademicYears\Schemas;
+
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class AcademicYearForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Select::make('school_id')
+                    ->relationship('school', 'name')
+                    ->required(),
+                DatePicker::make('start_date')
+                    ->required(),
+                DatePicker::make('end_date')
+                    ->required(),
+                TextInput::make('status')
+                    ->required(),
+            ]);
+    }
+}
